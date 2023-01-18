9594c6b22e99c240bf7a984f2af17ad8f50e072c
Eitan Moed <enmoed@gmail.com>
2023-01-19 01:04:16 +0200

    syntax

--- supplied_material/SyntaxValidatorTest.java
+++ supplied_material/SyntaxValidatorTest.java
@@ -8,63 +8,182 @@ import static org.junit.Assert.assertTrue;
 
 public class SyntaxValidatorTest {
     @Test
-    public void testGetLine_ifStatement() {
-        String input = "   if (  x < y) {";
-        List<String> expected = List.of("if", "(", "x < y",")", "{");
+    public void testGetLine_Statement() {
+        String input = " if ( x || y ) { ";
+        List<String> expected = List.of("if", "(", "x", "||", "y",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "if(x||y){";
+        expected = List.of("if", "(", "x", "||", "y",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "if (x&&y) {";
+        expected = List.of("if", "(", "x", "&&", "y",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "if (x) {";
+        expected = List.of("if", "(", "x",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "while (.2) {";
+        expected = List.of("while", "(", ".2",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "if (true) {";
+        expected = List.of("if", "(", "true",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "while (false) {";
+        expected = List.of("while", "(", "false",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = " if( x  &&  y ){ ";
+        expected = List.of("if", "(", "x", "&&", "y",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = " if( x  &&y|| .2){ ";
+        expected = List.of("if", "(", "x", "&&", "y", "||", ".2",")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = " if( x  &&|| a){ ";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = " if( ){ ";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = " while ( || a)   { ";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "if  (a &&){ ";
+        expected = List.of();
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
-    @Test
-    public void testGetLine_whileStatement() {
-        String input = "  while (  x > y  )   {  ";
-        List<String> expected = List.of("while", "(", "x > y",")", "{");
-        assertEquals(expected, SyntaxValidator.getLine(input));
-    }
+
     @Test
     public void testGetLine_functionStatement() {
-        String input = " void a_funcname    ( happy   happy ) {  ";
-        List<String> expected = List.of("void", "a_funcname", "(", "happy   happy", ")", "{");
+        String input = " void func(int a,   boolean   a  ,char a,String a , double a){  ";
+        List<String> expected = List.of("void", "func", "(", "int", "a", ",", "boolean", "a", ",",
+                "char", "a", ",", "String", "a", ",", "double", "a", ")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = " void func(){";
+        expected = List.of("void", "func", "(", ")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "void func_a ( ) {";
+        expected = List.of("void", "func_a", "(", ")", "{");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "void func ( int) {";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "void func ( var) {";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "void func ( int ,) {";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "void func ( , int) {";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "void func ( int .. ) {";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+    }
+    @Test
+    public void testGetLine_return() {
+        String input = "  return ;  ";
+        List<String> expected = List.of("return", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "return;";
+        expected = List.of("return", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "return  ;";
+        expected = List.of("return", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "return;  ";
+        expected = List.of("return", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "return  ";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+    }
+    @Test
+    public void testGetLine_closeBrackets() {
+        String input = "  }  ";
+        List<String> expected = List.of("}");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "}";
+        expected = List.of("}");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = " }";
+        expected = List.of("}");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "} ";
+        expected = List.of("}");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "; }  ";
+        expected = List.of();
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
     @Test
     public void testGetLine_methodStatement() {
-        String input = "  func   ( blah, blah ) ;  ";
-        List<String> expected = List.of("func", "(", "blah",",", "blah", ")", ";");
+        String input = "func ( a, b ) ;  ";
+        List<String> expected = List.of("func", "(", "a",",", "b", ")", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "func ( a, b ) ;  ";
+        expected = List.of("func", "(", "a",",", "b", ")", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "func( a, b );";
+        expected = List.of("func", "(", "a",",", "b", ")", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "func( );";
+        expected = List.of("func", "(", ")", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "func ( a, ) ;  ";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "func ( , b ) ;  ";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "func ( , ) ;  ";
+        expected = List.of();
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "func () ;  ";
+        expected = List.of("func", "(", ")", ";");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+    }
+    @Test
+    public void testGetLine_comment() {
+        String input = "\\\\ comment ";
+        List<String> expected = List.of("\\\\");
+        assertEquals(expected, SyntaxValidator.getLine(input));
+        input = "  \\\\  ";
+        expected = List.of();
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
     @Test
     public void testGetLine_intDec() {
-        String input = "  int   a =  5, b = +7, c ; ";
+        String input = "  int   a =  5 ,b=+7, c ; ";
         List<String> expected = List.of("int", "a", "=", "5", ",", "b", "=", "+7", ",", "c", ";");
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
     @Test
     public void testGetLine_doubleDec() {
-        String input = "  double   a =  5.0, b = +7, c ; ";
+        String input = "double   a =5.0 ,b = +7, c;";
         List<String> expected = List.of("double", "a", "=", "5.0", ",", "b", "=", "+7", ",", "c", ";");
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
     @Test
     public void testGetLine_StringDec() {
-        String input = "  String   a =  \"abc\", b = \"+7\", c ; ";
+        String input = "  String   a=\"abc\", b = \"+7\" ,c ; ";
         List<String> expected = List.of("String", "a", "=", "\"abc\"", ",", "b", "=", "\"+7\"", ",", "c", ";");
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
     @Test
     public void testGetLine_boolDec() {
-        String input = "  boolean   a, a =  true, b = 5., c ; ";
+        String input = "  boolean   a, a=true, b = 5. , c ; ";
         List<String> expected = List.of("boolean", "a",",", "a", "=", "true", ",", "b", "=", "5.", ",", "c", ";");
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
     @Test
     public void testGetLine_charDec() {
-        String input = "  char   a, a =  '.', b = '!', c ; ";
+        String input = "  char   a,a='.', b = '!', c ; ";
         List<String> expected = List.of("char", "a",",", "a", "=", "'.'", ",", "b", "=", "'!'", ",", "c", ";");
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
     @Test
     public void testGetLine_assign() {
-        String input = "  a =  '.', b = '!'; ";
-        List<String> expected = List.of( "a", "=", "'.'", ",", "b", "=", "'!'", ";");
+        String input = "  a='.', b=7; ";
+        List<String> expected = List.of( "a", "=", "'.'", ",", "b", "=", "7", ";");
         assertEquals(expected, SyntaxValidator.getLine(input));
     }
     @Test
@@ -72,4 +191,5 @@ public class SyntaxValidatorTest {
         String input = "invalid statement";
         assertTrue(SyntaxValidator.getLine(input).isEmpty());
     }
+
 }
