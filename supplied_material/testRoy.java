9cc9dcdfabbef9ef733057c9995da459fe8fc481
Roy Abend <roy@evolution.inc>
2023-01-21 14:12:51 +0200

    tests

--- supplied_material/testRoy.java
+++ supplied_material/testRoy.java
@@ -2,6 +2,12 @@ import oop.ex6.parser.Parser;
 import oop.ex6.trackers.FuncTracker;
 import oop.ex6.trackers.VarTracker;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class testRoy {
     private static String[] tests = {"/Users/roy/Documents/university/oop/sJavaInspector/supplied_material/tests/test003.sjava",
             "/Users/roy/Documents/university/oop/sJavaInspector/supplied_material/tests/test007.sjava",
@@ -25,8 +31,37 @@ public class testRoy {
             "/Users/roy/Documents/university/oop/sJavaInspector/supplied_material/tests/test501.sjava",
     };
 
+    private static String[] royTests = {"/Users/roy/Documents/university/oop/sJavaInspector/supplied_material/tests/t_roy_1.sjava",
+            "/Users/roy/Documents/university/oop/sJavaInspector/supplied_material/tests/t_roy_2.sjava",
+            "/Users/roy/Documents/university/oop/sJavaInspector/supplied_material/tests/t_roy_3.sjava",
+            "/Users/roy/Documents/university/oop/sJavaInspector/supplied_material/tests/t_roy_4.sjava",
+    };
+
+    public static List<String> getFilesStartingWith(String folderPath, String prefix) {
+        List<String> filePaths = new ArrayList<String>();
+        File folder = new File(folderPath);
+
+        for (File file : folder.listFiles()) {
+            if (file.isFile() && file.getName().startsWith(prefix)) {
+                filePaths.add(file.getAbsolutePath());
+            }
+        }
+
+        Collections.sort(filePaths, new Comparator<String>() {
+            @Override
+            public int compare(String file1, String file2) {
+                String[] parts1 = file1.split("_");
+                String[] parts2 = file2.split("_");
+                int n1 = Integer.parseInt(parts1[parts1.length - 1].replace(".sjava",""));
+                int n2 = Integer.parseInt(parts2[parts2.length - 1].replace(".sjava",""));
+                return n1 - n2;
+            }
+        });
+        return filePaths;
+    }
+
     public static void main(String[] args) {
-        for (var t : tests) {
+        for (var t : getFilesStartingWith("/Users/roy/Documents/university/oop/sJavaInspector/supplied_material/tests","t_roy_")) {
             try {
                 VarTracker.reset();
                 FuncTracker.reset();
